7fe928b70fb7fea478c3a1645768bfcc443bf69c
Khuthadzo TSHIKOTSHI <ktshikot@e5r1p4.jnb.42.fr>
2017-01-10 13:24:35 +0200

    clean up header

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -21,26 +21,8 @@
 typedef struct	s_printret
 {
 	int         returnvalue;
-	char		*basevalue;
+	char		*outvalue;
 }				t_printret;
 
-void	handle_ll(char format, va_list argp, t_printret *ret);
-void	handle_hh(char format, va_list argp, t_printret *ret);
-void	handle_h(char format, va_list argp, t_printret *ret);
-void	handle_j(char format, va_list argp, t_printret *ret);
-void	handle_z(char format, va_list argp, t_printret *ret);
-int		handle_length(const char *format, va_list argp, t_printret *ret);
-void	base(unsigned int number_to_convert, char case_, int base, t_printret *ret);
-void	base_l(unsigned long number_to_convert, char case_, int base, t_printret *ret);
-void	base_change(va_list argp, char format, t_printret *ret);
-void	print_string(va_list argp, char format, t_printret *ret);
-int	    handle_num(va_list argp, char format, int pos_num, t_printret *ret);
-void	pointer_s(va_list argp, char format, t_printret *ret);
-int		double_percent(char format);
-int		handle_hashtag(char format, t_printret *rt, va_list argp);
-int		handle_0_padding(const char *format);
-int		handle_space_padding(const char *format);
-int		handle_padding(const char *format, va_list argp, t_printret *ret);
 int		ft_printf(const char *format, ...);
-
 #endif
