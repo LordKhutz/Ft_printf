b7419488e8c9946e803d32594785a4a9f9fac575
Khuthadzo TSHIKOTSHI <ktshikot@e5r5p3.jnb.42.fr>
2017-01-13 12:07:45 +0200

    modified ft_display

--- src/ft_display.c
+++ src/ft_display.c
@@ -2,14 +2,14 @@
 
 void	ft_ljf(t_printret *ret)
 {
-    int i;
+	int i;
 
-    i = 0;
+	i = 0;
 	if (ret->padding_num <= ret->outval_num)
-		{
-			ft_putstr(ret->outvalue);
-			ret->chars_printed += ret->outval_num;
-		}
+	{
+		ft_putstr(ret->outvalue);
+		ret->chars_printed += ret->outval_num;
+	}
 	else
 	{
 		while (ret->padding_num--)
@@ -25,25 +25,25 @@ void	ft_ljf(t_printret *ret)
 
 void	ft_rjf(t_printret *ret)
 {
-    int i;
-    char    *s;
+	int i;
+	char    *s;
 
-    i = 0;
+	i = 0;
 	if (ret->padding_num <= ret->outval_num)
-		{
-			while (i < ret->outval_num)
-                ft_putchar(ret->outvalue[i++]);
-			ret->chars_printed += ret->outval_num;
-		}
+	{
+		while (i < ret->outval_num)
+			ft_putchar(ret->outvalue[i++]);
+		ret->chars_printed += ret->outval_num;
+	}
 	else
 	{
-	    s = ret->outvalue;
-	    if (ret->padding_type == 48 && (*s == '-' || *s == '+'))
-            ft_putchar(*s++);
-        ret->padding_num -= ret->outval_num;
-        ret->chars_printed += ret->padding_num+ ret->outval_num;
+		s = ret->outvalue;
+		if (ret->padding_type == 48 && (*s == '-' || *s == '+'))
+			ft_putchar(*s++);
+		ret->padding_num -= ret->outval_num;
+		ret->chars_printed += ret->padding_num+ ret->outval_num;
 		while (ret->padding_num--)
-            ft_putchar(ret->padding_type);
+			ft_putchar(ret->padding_type);
 		ft_putstr(s);
 	}
 }
@@ -52,16 +52,16 @@ void	ft_display(t_printret *ret)
 {
 	ret->outval_num = ft_strlen(ret->outvalue);
 	if (ret->isnumber == 1 && ret->precision > 0)
-    {
-        if (ret->precision > ret->outval_num)
-        {
-            ret->padding_type = '0';
-            ret->padding_num += ret->precision - ret->outval_num;
-        }
-    }
-    else if (ret->isnumber == 0 && ret->precision > -1)
-        if (ret->outval_num > ret->precision)
-            ret->outval_num = ret->precision;
+	{
+		if (ret->precision > ret->outval_num)
+		{
+			ret->padding_type = '0';
+			ret->padding_num += ret->precision - ret->outval_num;
+		}
+	}
+	else if (ret->isnumber == 0 && ret->precision > -1)
+		if (ret->outval_num > ret->precision)
+			ret->outval_num = ret->precision;
 	if (ret->padding_justify == -1)
 		ft_ljf(ret);
 	else
